ac3f1d4ad4d8b4e154ec7993a9810bab0bb87976
Lidia Safiullina <lidiia.safiullina@yandex.ru>
2018-05-03 20:56:17 -0700

    Introduced function to recognize HEX coded colors.

--- drawShape.js
+++ drawShape.js
@@ -28,9 +28,100 @@ function colorRGBChecker(val) {
     } else if (colorRGBPartCheckUp !== null) {
         return val = "rgb(" + val + ")";
     } else {
-        return val;
+        return colorHEXChecker(val);
     }
 }
+
+//Stores the HEX to number values
+function switchHexToNumbers(v) {
+    switch (v) {
+    case "0":
+        return "0";
+    case "1":
+        return "1";
+    case "2":
+        return "2";
+    case "3":
+        return "3";
+    case "4":
+        return "4";
+    case "5":
+        return "5";
+    case "6":
+        return "6";
+    case "7":
+        return "7";
+    case "8":
+        return "8";
+    case "9":
+        return "9";
+    case "a":
+        return "10";
+    case "b":
+        return "11";
+    case "c":
+        return "12";
+    case "d":
+        return "13";
+    case "e":
+        return "14";
+    case "f":
+        return "15";
+    }
+}
+
+
+function colorHEXChecker(value) {
+    //Finishes the function if the amount of characters is not equal to 3 or 6
+    if (value.length !== 3 && value.length !== 6) {
+        return value;
+    } else {
+        //Splits the string into array
+        var colorValueArr = value.split("");
+        //Transfers HEX value into numbers
+        var numberValueArr = colorValueArr.map(function(b){
+            return switchHexToNumbers(b);
+        });
+        //Loops through array to get multiplied number
+        var multipliedNumberValueArr = [];
+        var finalArray = [];
+        for (var i = 0; i < numberValueArr.length; ++i) {
+            //Checks whether the element in array is undefined and returns initial value in this case
+            if (!numberValueArr[i]) {
+                return value;
+            } else {
+                var dividend = i + 2;
+                var modulus = dividend % 2;
+                if (modulus % 2 === 0) {
+                    var x = numberValueArr[i] * Math.pow(16, 1);
+                    multipliedNumberValueArr.push(x);
+                } else {
+                    var z = numberValueArr[i] * Math.pow(16, 0);
+                    multipliedNumberValueArr.push(z);
+                }
+            }
+        }
+        //Loops through array to get three number (RGB-styled) array
+        for (var k = 0; k < multipliedNumberValueArr.length; ++k) {
+            var dividendTwo = k + 2;
+            var modulusTwo = dividendTwo % 2;
+            if (modulusTwo % 2 === 0) {
+                var j = k + 1;
+                var y = multipliedNumberValueArr[k] + multipliedNumberValueArr[j];
+                finalArray.push(y);
+            }
+        }
+        for (var l = 0; l < finalArray.length; ++l) {
+            //Returns the initial value if one element in array is undefined
+            if (finalArray[l] == "NaN") {
+                return value;
+            }
+            return "#" + value;
+        }
+    }
+}
+
+
 
 //Changes the form of a square into a circle
 function makeCircle() {
@@ -51,7 +142,3 @@ var color = document.getElementById("color");
 color.addEventListener("blur", changeColor);
 var checkBox = document.getElementById("checkBox");
 checkBox.addEventListener("click", makeCircle);
-
-
-
-
